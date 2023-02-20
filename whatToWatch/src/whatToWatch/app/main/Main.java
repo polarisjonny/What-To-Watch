bbb63a0c041d0ce87962907b420f202dddcfb779
seyeon ji <joeun9705@gmail.com>
2023-02-20 17:47:01 +0900

    feat: add features of scanner system

--- whatToWatch/src/whatToWatch/app/main/Main.java
+++ whatToWatch/src/whatToWatch/app/main/Main.java
@@ -1,10 +1,13 @@
 package whatToWatch.app.main;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static final Scanner SC = new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
 		//여기에다가 작성하지 말것
 
 	}
-
 }
