7cae46af30299b96e33307ca2f75e51d9ccd7797
inhok-windows11\inhok <kcjkcj0621!>
2023-02-20 17:39:20 +0900

    테스트1

---
+++ whatToWatch/src/miniPrj/cj/freeBoard/FreeBoardMain.java
@@ -0,0 +1,22 @@
+package miniPrj.cj.freeBoard;
+
+public class FreeBoardMain {
+	
+	public void boardMain() {
+		
+		System.out.println("====자유게시판====");
+		
+		System.out.println("1. 게시판 작성");
+		System.out.println("2. 게시판 조회");
+		System.out.println("3. 게시판 수정");
+		System.out.println("4. 게시판 삭제");
+		
+		//게시판 목록 ~~~
+		
+		
+		
+		
+		
+	}
+
+}
