150a5fb18ea5d68d8f424b4ff711027c477f3102
김예진 <>
2023-02-22 20:29:48 +0900

    커밋

--- whatToWatch/src/com/team2/wtw/manager/Menu.java
+++ whatToWatch/src/com/team2/wtw/manager/Menu.java
@@ -69,9 +69,9 @@ public class Menu {
 		if (isManager) {
 			while (isFinish) {
 				printMenu();
-				int choice;
+				String choice;
 				try {
-					choice = sc.nextInt();
+					choice = sc.nextLine();
 				} catch (InputMismatchException e) {
 					System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
 					sc.next(); // 입력
@@ -79,10 +79,10 @@ public class Menu {
 				}
 
 				switch (choice) {
-				case 1:
+				case "1":
 					isFinish = false;
 					break;
-				case 2: /* 회원 관리 */
+				case "2": /* 회원 관리 */
 					int permissionNo = jt.getPermissionNo(username);
 					if (permissionNo == 1 || permissionNo == 2) {
 						memberManagementMenu();
@@ -90,7 +90,7 @@ public class Menu {
 						System.out.println("\n접속 권한이 없습니다.");
 					}
 					break;
-				case 3: /* 게시판 관리 */
+				case "3": /* 게시판 관리 */
 					permissionNo = JdbcTemplate.getPermissionNo(username);
 					if (permissionNo == 1 || permissionNo == 2 || permissionNo == 3) {
 						boardManagementMenu();
@@ -98,7 +98,7 @@ public class Menu {
 						System.out.println("\n접속 권한이 없습니다.");
 					}
 					break;
-				case 4: /* 리뷰 관리 */
+				case "4": /* 리뷰 관리 */
 					permissionNo = JdbcTemplate.getPermissionNo(username);
 					if (permissionNo == 1 || permissionNo == 2 || permissionNo == 4) {
 						reviewManagementMenu();
@@ -106,7 +106,7 @@ public class Menu {
 						System.out.println("\n접속 권한이 없습니다.");
 					}
 					break;
-				case 5: /* 문의 관리 */
+				case "5": /* 문의 관리 */
 					permissionNo = JdbcTemplate.getPermissionNo(username);
 					if (permissionNo == 1 || permissionNo == 6) {
 						questionManagementMenu();
@@ -114,7 +114,7 @@ public class Menu {
 						System.out.println("\n접속 권한이 없습니다.");
 					}
 					break;
-				case 6: /* 이벤트 관리 */
+				case "6": /* 이벤트 관리 */
 					permissionNo = JdbcTemplate.getPermissionNo(username);
 					if (permissionNo == 1 || permissionNo == 7) {
 						eventManagementMenu();
@@ -122,7 +122,7 @@ public class Menu {
 						System.out.println("\n접속 권한이 없습니다.");
 					}
 					break;
-				case 7: /* 컨텐츠 관리 */
+				case "7": /* 컨텐츠 관리 */
 					contentsManagement();
 					break;
 				default:
@@ -142,18 +142,18 @@ public class Menu {
 		System.out.println("2. 제재회원");
 		System.out.println("3. 관리자");
 		System.out.print("메뉴 선택 : ");
-		int memberType = sc.nextInt();
+		String memberType = sc.nextLine();
 		switch (memberType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			us.getAllUser();
 			break;
-		case 2:
+		case "2":
 			mm.sanctionMember();
 			break;
-		case 3:
+		case "3":
 			mm.manager();
 			break;
 		default:
@@ -169,17 +169,17 @@ public class Menu {
 		System.out.println("0. 관리자 홈");
 		System.out.println("1. 게시판 관리");
 		System.out.println("2. 제재 게시판 목록");
-		int boardType = sc.nextInt();
+		String boardType = sc.nextLine();
 		switch (boardType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			// 게시판 목록 조회 후 작성자 번호 신고하기
 			fbs.BoardList();
 			sm.inputSanction();
 			break;
-		case 2:
+		case "2":
 			sm.printBoardSanctions(jt.getConnection());
 			break;
 		}
@@ -193,16 +193,16 @@ public class Menu {
 		System.out.println("0. 관리자 홈");
 		System.out.println("1. 리뷰 관리");
 		System.out.println("2. 제재 리뷰 목록");
-		int reviewType = sc.nextInt();
+		String reviewType = sc.nextLine();
 		switch (reviewType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			rm.printReviewList();
 			sm.inputSanction();
 			break;
-		case 2:
+		case "2":
 			sm.printReviewSanctions(jt.getConnection());
 			break;
 		}
@@ -218,15 +218,15 @@ public class Menu {
 		System.out.println("1. 자주묻는질문");
 		System.out.println("2. 문의게시판");
 		System.out.print("메뉴 선택 : ");
-		int questionType = sc.nextInt();
+		String questionType = sc.nextLine();
 		switch (questionType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			ps.faqPlayStart();
 			break;
-		case 2:
+		case "2":
 			// 문의게시판 확인
 			ps.qaPlayStart();
 			// 문의게시판 답변
@@ -245,16 +245,16 @@ public class Menu {
 		System.out.println("0. 관리자 홈");
 		System.out.println("1. 이벤트 추가/삭제/수정");
 		System.out.println("2. 점수 조회");
-		int eventType = sc.nextInt();
+		String eventType = sc.nextLine();
 
 		switch (eventType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			em.inputAddEvent();
 			break;
-		case 2:
+		case "2":
 			em.printEventScore();
 			break;
 		}
@@ -271,23 +271,22 @@ public class Menu {
 		System.out.println("2. 예능 추가/수정/삭제");
 		System.out.println("3. 영화 추가/수정/삭제");
 		System.out.print("메뉴 선택 : ");
-		int contentsType = sc.nextInt();
+		String contentsType = sc.nextLine();
 
 		switch (contentsType) {
-		case 0:
+		case "0":
 			choiceMenu();
 			break;
-		case 1:
+		case "1":
 			dc.showDramaContents();
 			cm.contentsManage();
 			break;
-		case 2:
+		case "2":
 			ec.showEntContents();
 			cm.contentsManage();
 			break;
-		case 3:
+		case "3":
 			mc.showMovieContents();
-
 			cm.contentsManage();
 			break;
 		}
